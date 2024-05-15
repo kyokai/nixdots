4f6c7dbe3d6d956ad402ca868216193b8e079dec
Redyf <mateusalvespereira7@gmail.com>
2024-05-15 10:40:50 +0000

    ags stuff

--- home/redyf/desktop/hyprland/default.nix
+++ home/redyf/desktop/hyprland/default.nix
@@ -1,6 +1,8 @@
 {
   pkgs,
   inputs,
+  config,
+  lib,
   ...
 }: let
   hyprlandFlake = inputs.hyprland.packages.${pkgs.system}.hyprland;
@@ -145,20 +147,7 @@ in {
         };
       };
 
-    services = {
-      gvfs.enable = true;
-      devmon.enable = true;
-      udisks2.enable = true;
-      upower.enable = true;
-      power-profiles-daemon.enable = true;
-      accounts-daemon.enable = true;
-      gnome = {
-        evolution-data-server.enable = true;
-        glib-networking.enable = true;
-        gnome-keyring.enable = true;
-        gnome-online-accounts.enable = true;
-      };
-    };
+
 
 
       animations = {
